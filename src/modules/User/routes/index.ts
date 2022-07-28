1d5884f67acf8a6f51929091f72cace4f5d1d1e9
Jonathan <jonathan.wpc@gmail.com>
2022-07-28 12:23:20 -0300

    Atualização na configuração das rotas

--- src/modules/User/routes/index.ts
+++ src/modules/User/routes/index.ts
@@ -1,26 +1,14 @@
 import { Router } from "express";
 import { userController } from "../controller";
 import userValidator from "../validators";
-import multer from "multer";
-import { v4 as uuid } from 'uuid';
-
-const upload = multer({
-  storage: multer.diskStorage({
-    destination: 'images/',
-    filename(req, file, callback) {
-      const fileName = `${uuid()}-${file.originalname}`
-
-      return callback(null, fileName)
-    },
-  }),
-})
+import upload from "../../../infra/config/multer";
 
 const routes = Router();
 
 routes.get("/users", userController.listAll());
 routes.get("/users/:id", userController.list());
-routes.post("/users", upload.single("profilePicture"),  userController.create()); // Criação da 1ª rota, cadastrar usuário.
-routes.put("/users/:id", upload.single("profilePicture"), userController.update());
+routes.post("/users", upload.single("profilePicture"), userValidator.create, userController.create()); // Criação da 1ª rota, cadastrar usuário.
+routes.put("/users/:id", upload.single("profilePicture"), userValidator.update, userController.update());
 routes.delete("/users/:id", userController.delete());
 
 
