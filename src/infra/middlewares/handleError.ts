ce15c8fd27010829b4e3d900ff89617f66c23595
Jonathan <jonathan.wpc@gmail.com>
2022-07-29 15:46:35 -0300

    Padronização das mensagens de Erros

--- src/infra/middlewares/handleError.ts
+++ src/infra/middlewares/handleError.ts
@@ -4,15 +4,22 @@ import AppError from "../../errors/AppError";
 
 const handleError = (error: Error, req: Request, res: Response, next: NextFunction) => {
   require("express-async-errors");
+  
   if (error instanceof ValidationError) {
-    return res.status(error.statusCode).json(error.details);
+    const responseMessage = error.details.body ? error.details.body[0].message : error.details;
+    return res
+      .status(error.statusCode)
+      .json({ statusCode: error.statusCode, message: responseMessage });
   }
 
   if(error instanceof AppError) {
-    return res.status(error.statusCode).json({ statusCode: error.statusCode, message: error.message });
+    return res
+      .status(error.statusCode).json({ statusCode: error.statusCode, message: error.message });
   }
 
-  return res.status(500).json({ statusCode: 500, message: "Internal Server Error" });
+  return res
+    .status(500)
+    .json({ statusCode: 500, message: "Internal Server Error" });
 };
 
 export default handleError;
