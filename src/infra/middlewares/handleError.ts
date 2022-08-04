d3c1a8440408099acb28c477eac7606ee0cc68c4
Jonathan <jonathan.wpc@gmail.com>
2022-08-03 23:44:43 -0300

    Atualização no middleware handleError

--- src/infra/middlewares/handleError.ts
+++ src/infra/middlewares/handleError.ts
@@ -4,22 +4,23 @@ import AppError from "../../errors/AppError";
 
 const handleError = (error: Error, req: Request, res: Response, next: NextFunction) => {
   require("express-async-errors");
-  
+
   if (error instanceof ValidationError) {
     const responseMessage = error.details.body ? error.details.body[0].message : error.details;
     return res
       .status(error.statusCode)
-      .json({ statusCode: error.statusCode, message: responseMessage });
+      .json({ error: true, statusCode: error.statusCode, message: responseMessage });
   }
 
   if(error instanceof AppError) {
     return res
-      .status(error.statusCode).json({ statusCode: error.statusCode, message: error.message });
+      .status(error.statusCode).json({ error: true, statusCode: error.statusCode, message: error.message });
   }
+  console.log(error);
 
   return res
     .status(500)
-    .json({ statusCode: 500, message: "Internal Server Error" });
+    .json({ error: true, statusCode: 500, message: "Internal Server Error" });
 };
 
 export default handleError;
